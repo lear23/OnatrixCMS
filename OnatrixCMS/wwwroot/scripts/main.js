8b30905e1439081ebf12c4070264d11936683b12
lear23 <130910243+lear23@users.noreply.github.com>
2024-09-17 08:25:33 +0200

    Add project files.

---
+++ OnatrixCMS/wwwroot/scripts/main.js
@@ -0,0 +1,50 @@
+// --------------------Services-----------------
+
+window.addEventListener('scroll', function() {
+    var servicesSection = document.querySelector('.services');
+    var servicesH5 = document.querySelector('.services h5');
+    var servicesH1 = document.querySelector('.services h1');
+    var servicesPosition = servicesSection.getBoundingClientRect().top;
+    var screenPosition = window.innerHeight / 1.1;
+
+   
+    if (servicesPosition < screenPosition && servicesPosition > -servicesSection.offsetHeight) {
+        servicesH5.classList.add('animate');
+        servicesH1.classList.add('animate');
+    } else {
+        servicesH5.classList.remove('animate');
+        servicesH1.classList.remove('animate');
+    }
+});
+
+// -------------------------video------------------------------
+
+
+const video = document.querySelector('.responsive-video');
+
+
+video.addEventListener('mouseover', function() {
+    video.play();
+});
+
+video.addEventListener('mouseout', function() {
+    video.pause();
+});
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+window.addEventListener('scroll', function() {
+    if (isInViewport(video)) {
+        video.play();
+    } else {
+        video.pause();
+    }
+});
+
