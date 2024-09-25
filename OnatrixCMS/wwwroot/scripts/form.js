cde26395627e10fa7a35028216c74adeb2c9554a
israel morales lara <130910243+lear23@users.noreply.github.com.>
2024-09-25 17:34:53 +0200

    fixade risk formulär nu måste jag fixa att förmulär stannar

--- OnatrixCMS/wwwroot/scripts/form.js
+++ OnatrixCMS/wwwroot/scripts/form.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             
            
-        }, 3000);
+        }, 4000);
         window.location.href = '#myForm'; 
         backButton.addEventListener('click', function () {
             successMessage.style.display = 'none';
@@ -33,35 +33,4 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-//---------------------RISKFORM----------------------------
-
-document.addEventListener("DOMContentLoaded", function () {
-    const successMessage = document.getElementById('successRiskMessage');
-    const contactForm = document.getElementById('riskForm');
-    const backButton = document.getElementById('backBtn');
-
-    if (successMessage) {
-        window.scrollTo(0, successMessage.offsetTop);
-
-        setTimeout(() => {
-            successMessage.style.display = 'none';
-            backButton.style.display = 'block';
-            if (contactForm) {
-                contactForm.reset();
-            }
-
-
-        }, 3000);
-        window.location.href = '#riskForm';
-        backButton.addEventListener('click', function () {
-            successMessage.style.display = 'none';
-            backButton.style.display = 'none';
-            if (contactForm) {
-                contactForm.reset();
-            }
-            window.location.href = '/';
-        });
-
-    }
-});
 
---
+++ OnatrixCMS/wwwroot/scripts/riskForm.js
@@ -0,0 +1,33 @@
+﻿
+//---------------------RISKFORM----------------------------
+
+document.addEventListener("DOMContentLoaded", function () {
+    const successMessage = document.getElementById('successRiskMessage');
+    const contactForm = document.getElementById('riskForm');
+    const backButton = document.getElementById('backBtn');
+
+    if (successMessage) {
+        window.scrollTo(0, successMessage.offsetTop);
+
+        setTimeout(() => {
+            successMessage.style.display = 'none';
+            backButton.style.display = 'block';
+            if (contactForm) {
+                contactForm.reset();
+            }
+
+
+        }, 3000);
+        window.location.href = '#riskForm';
+        backButton.addEventListener('click', function () {
+            successMessage.style.display = 'none';
+            backButton.style.display = 'none';
+            if (contactForm) {
+                contactForm.reset();
+            }
+            
+        });
+
+    }
+});
+
