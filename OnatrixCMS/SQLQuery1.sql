351840169c562a8ee94cf87fbb06a2470c503d6a
israel morales lara <130910243+lear23@users.noreply.github.com.>
2024-12-22 14:02:49 +0100

    fixade database, nu fungera den

---
+++ OnatrixCMS/SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿-- Crear un nuevo login en SQL Server
+CREATE LOGIN NuevoUsuario WITH PASSWORD = 'NuevaContraseña123';
+
+
+USE OnatrixCMS;
+
+-- Crear un usuario para la base de datos
+CREATE USER NuevoUsuario FOR LOGIN NuevoUsuario;
+
+-- Asignar permisos al nuevo usuario (asegúrate de que tenga permisos para leer y escribir)
+ALTER ROLE db_owner ADD MEMBER NuevoUsuario;
+
